dc17d187e410ee14f7b2b66ae6a5092fa8d0119b
Kevin Haraux <kevin.haraux@gmail.com>
2022-10-26 23:52:20 +0200

    final

--- repmois.php
+++ repmois.php
@@ -1,7 +1,64 @@
 <?php
-if ($_POST) {
-    if ($_POST['nombre']='1') {
-    echo "Janvier 31 jours";
-    }
-    }
+        if($_POST) {
+        if(isset($_POST['01'])) {
+            echo "Janvier 31 jours<br/><br/>";
+            }
+            }
+
+                if($_POST) {
+                    if(isset($_POST['02'])) {
+                        echo "Fevrier 28 jours<br/><br/>";
+                    }
+                    }
+
+                        if($_POST) {
+                            if(isset($_POST['03'])) {
+                                echo "Mars 31 jours<br/><br/>";
+                            }
+                            }
+                                if($_POST) {
+                                    if(isset($_POST['04'])) {
+                                        echo "Avril 30 jours<br/><br/>";
+                                        }
+                                        }
+                                            if($_POST) {
+                                                if(isset($_POST['05'])) {
+                                                    echo "Mai 31 jours<br/><br/>";
+                                                    }
+                                                    }
+                                                        if($_POST) {
+                                                            if(isset($_POST['06'])) {
+                                                                echo "Juin 30 jours<br/><br/>";
+                                                                }
+                                                                }
+                        if($_POST) {
+                            if(isset($_POST['07'])) {
+                                echo "Juillet 31 jours<br/><br/>";
+                                }
+                                }
+                                if($_POST) {
+                                    if(isset($_POST['08'])) {
+                                        echo "Aout 31 jours<br/><br/>";
+                                        }
+                                        }
+                                        if($_POST) {
+                                            if(isset($_POST['09'])) {
+                                                echo "Septembre 30 jours<br/><br/>";
+                                                }
+                                                }
+                                                if($_POST) {
+                                                    if(isset($_POST['10'])) {
+                                                        echo "Octobre 31 jours<br/><br/>";
+                                                        }
+                                                        }
+                                                        if($_POST) {
+                                                            if(isset($_POST['11'])) {
+                                                                echo "Novembre 30 jours<br/><br/>";
+                                                                }
+                                                                }
+                                                                if($_POST) {
+                                                                    if(isset($_POST['12'])) {
+                                                                        echo "Decembre 31 jours<br/><br/>";
+                                                                        }
+                                                                        }
 ?>
