4a53d11e600aa22f9dcd35ad6dc7c1b1d74b285a
Kevin Haraux <kevin.haraux@gmail.com>
2022-10-26 23:32:27 +0200

    php2

--- repmois.php
+++ repmois.php
@@ -1,75 +1,7 @@
-<?php 
-
-if($_POST) {
-    if($_POST['number']=='1') {
-    $mot = "Janvier 31 jours<br/>";
+<?php
+if ($_POST) {
+    if ($_POST['nombre']='1') {
+    echo "Janvier 31 jours";
     }
-    echo "" . $mot ."";
     }
-    if($_POST) {
-        if($_POST['number']=='2') {
-        $mot = "Fevrier 28 jours<br/>";
-        }
-        echo "" . $mot ."";
-        }
-        if($_POST) {
-            if($_POST['number']=='3') {
-            $mot = "Mars 31 jours<br/>";
-            }
-            echo "" . $mot ."";
-            }
-            if($_POST) {
-                if($_POST['number']=='4') {
-                $mot = "Avril 30 jours<br/>";
-                }
-                echo "" . $mot ."";
-                }
-                if($_POST) {
-                    if($_POST['number']=='5') {
-                    $mot = "Mai 31 jours<br/>";
-                    }
-                    echo "" . $mot ."";
-                    }
-                    if($_POST) {
-                        if($_POST['number']=='6') {
-                        $mot = "Juin 30 jours<br/>";
-                        }
-                        echo "" . $mot ."";
-                        }
-                        if($_POST) {
-                            if($_POST['number']=='7') {
-                            $mot = "Juillet 31 jours<br/>";
-                            }
-                            echo "" . $mot ."";
-                            }
-                            if($_POST) {
-                                if($_POST['number']=='8') {
-                                $mot = "Aout 31 jours<br/>";
-                                }
-                                echo "" . $mot ."";
-                                }
-                                if($_POST) {
-                                    if($_POST['number']=='9') {
-                                    $mot = "Septembre 30 jours<br/>";
-                                    }
-                                    echo "" . $mot ."";
-                                    }
-                                    if($_POST) {
-                                        if($_POST['number']=='10') {
-                                        $mot = "Octobre 31 jours<br/>";
-                                        }
-                                        echo "" . $mot ."";
-                                        }
-                                        if($_POST) {
-                                            if($_POST['number']=='11') {
-                                            $mot = "Novembre 30 jours<br/>";
-                                            }
-                                            echo "" . $mot ."";
-                                            }
-                                            if($_POST) {
-                                                if($_POST['number']=='12') {
-                                                $mot = "Decembre 31 jours<br/>";
-                                                }
-                                                echo "" . $mot ."";
-                                                }
 ?>
